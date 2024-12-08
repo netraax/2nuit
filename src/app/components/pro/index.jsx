cd31e1fb25380aefd8cd51611d51640a5a572c12
netraax <dils389@gmail.com>
2024-12-08 01:23:47 +0100

    Create index.jsx

---
+++ src/app/components/pro/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// Ce fichier contiendra les composants spécifiques à l'analyse pro
+export const ProAnalysis = () => {
+  return (
+    <div>
+      {/* Pro analysis components will go here */}
+    </div>
+  );
+};
