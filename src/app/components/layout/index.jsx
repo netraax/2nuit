fbed3d06186d7ea61430c372d75589830dedf9dd
netraax <dils389@gmail.com>
2024-12-08 01:18:33 +0100

    Create index.jsx

---
+++ src/app/components/layout/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const Header = () => {
+  return (
+    <header className="bg-vinted-primary text-white">
+      {/* Header content will go here */}
+    </header>
+  );
+};
+
+export const Footer = () => {
+  return (
+    <footer>
+      {/* Footer content will go here */}
+    </footer>
+  );
+};
