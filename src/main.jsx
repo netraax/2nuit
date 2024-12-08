3147b93b160141960d095dd6ae5c40c819bbca1f
netraax <dils389@gmail.com>
2024-12-08 02:29:08 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './app/App'; // Import du composant App depuis src/app/App.jsx
 
-document.body.style.backgroundColor = 'white'
+document.body.style.backgroundColor = 'white';
 
-const app = (
-  <h1 style={{
-    color: 'black',
-    fontSize: '30px',
-    margin: '20px'
-  }}>
-    VINITED
-  </h1>
-)
-
-ReactDOM.createRoot(document.getElementById('root')).render(app)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App /> {/* Utilisation du composant App */}
+  </React.StrictMode>
+);
