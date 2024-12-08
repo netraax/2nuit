6b4ae5506b77613136a45a485ddc566c4b3942aa
netraax <dils389@gmail.com>
2024-12-08 02:35:49 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app/App'; // Import du composant App depuis src/app/App.jsx
 
-document.body.style.backgroundColor = 'white';
+// Changer l'arrière-plan en noir
+document.body.style.backgroundColor = 'black';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
