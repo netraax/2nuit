34ebd826b599ab723ab215aec3d0962b11309ea6
netraax <dils389@gmail.com>
2024-12-08 01:27:01 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import App from './app/App.jsx'
+import './styles/output.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <div>Vinited - En construction</div>
+    <App />
   </React.StrictMode>
 )
