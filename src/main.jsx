d11d0234d6c7a0f9a683dc8d26b0e99f982ef3e8
netraax <dils389@gmail.com>
2024-12-08 01:56:00 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <div style={{
-      color: 'black',
-      fontSize: '24px',
-      padding: '20px',
-      backgroundColor: 'white'
-    }}>
-      Test Vinited
-    </div>
-  </React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <h1>Vinited</h1>
 )
