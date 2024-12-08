2bc5044af860264df2a4fe5c728026962cd306d3
netraax <dils389@gmail.com>
2024-12-08 01:52:19 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './app/App'
-import './styles/input.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <div style={{
+      color: 'black',
+      fontSize: '24px',
+      padding: '20px',
+      backgroundColor: 'white'
+    }}>
+      Test Vinited
+    </div>
   </React.StrictMode>
 )
