32c70a3eb3eafbe6f70c8ffbce63d33b2f7a2dbc
netraax <dils389@gmail.com>
2024-12-08 01:20:30 +0100

    Create index.jsx

---
+++ src/app/components/input/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// Ce fichier contiendra les composants pour la zone de texte et les boutons
+export const TextInput = () => {
+  return (
+    <div>
+      {/* Text input component will go here */}
+    </div>
+  );
+};
