46c91c30ef6e7c3e9ce3cb73d714244ddbc520a6
netraax <dils389@gmail.com>
2024-12-08 01:22:00 +0100

    Create index.jsx

---
+++ src/app/components/analysis/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// Ce fichier contiendra les composants pour afficher les résultats d'analyse
+export const AnalysisResults = () => {
+  return (
+    <div>
+      {/* Analysis results will go here */}
+    </div>
+  );
+};
