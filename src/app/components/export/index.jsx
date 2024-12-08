83438cbdc2354a70d64d584c89a0c6e69d1c703c
netraax <dils389@gmail.com>
2024-12-08 01:22:39 +0100

    Create index.jsx

---
+++ src/app/components/export/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// Ce fichier contiendra les composants pour l'export PDF
+export const PDFExport = () => {
+  return (
+    <div>
+      {/* PDF export components will go here */}
+    </div>
+  );
+};
