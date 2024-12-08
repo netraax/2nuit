7e73948a09a95c4627748fa5030142c91381fd38
netraax <dils389@gmail.com>
2024-12-08 01:45:20 +0100

    Update App.jsx

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 function App() {
   return (
-    <div>
-      <h1>Vinited - En construction</h1>
+    <div className="min-h-screen bg-white">
+      <div className="container mx-auto p-4">
+        <h1 className="text-4xl font-bold text-black">
+          Vinited - En construction
+        </h1>
+      </div>
     </div>
   )
 }
