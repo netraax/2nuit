2add14651f1698381318e5c1edb60af10dfaf97f
netraax <dils389@gmail.com>
2024-12-08 01:21:11 +0100

    Create index.jsx

---
+++ src/app/components/charts/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// Ce fichier contiendra les composants pour les graphiques
+export const SalesChart = () => {
+  return (
+    <div>
+      {/* Charts components will go here */}
+    </div>
+  );
+};
